37998ada5790371fc8e3453035c8f5b1bf7a4f18
hortty <gabrielhortmann@gmail.com>
2023-04-12 01:26:08 -0300

    Created Angular project base

--- Movie-Angular/src/app/app.module.ts
+++ Movie-Angular/src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+import { IndexComponent } from './index/index.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MatIconModule } from '@angular/material/icon';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubscribeComponent,
+    IndexComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    MatIconModule,
+    MatButtonModule
   ],
-  providers: [],
+  exports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatIconModule
+  ],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Movie-Angular/src/app/subscribe/subscribe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'subscribe',
+  templateUrl: './subscribe.component.html',
+  styleUrls: ['./subscribe.component.scss']
+})
+export class SubscribeComponent {
+
+}
